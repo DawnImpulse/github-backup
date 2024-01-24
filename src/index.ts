cfcb900e6c900474cf7133e4a94b3fad58b9424f
Saksham Khurana <dawnimpulse@gmail.com>
2024-01-24 18:54:47 +0530

    feat: compress result to zip

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import fileName from "./fileName";
 import repolist from "./repolist";
 import Rest from "./rest";
 import download from "./download";
+import compress from "./compress";
+import { info, success, error } from "log-symbols";
 
 const program = new Command();
 const moreInfo = "more info at https://github.com/dawnimpulse/github-backup";
@@ -17,30 +19,59 @@ program
     .option(
         "-n, --name <char>",
         `provide filename (optional); you can include variables; ${moreInfo}`,
-        "backup-{YYYY-MM-DD-HH-mm}"
+        "backup-{YYYY-MM-DD-HH-mm-ss}"
     )
-    .option("-t, --token <char>", `(required) user github token; ${moreInfo}`);
+    .option("-t, --token <char>", `(required) user github token; ${moreInfo}`)
+    .option(
+        "-p, --path <char>",
+        "(optional) output directory; default is current directory",
+        ""
+    );
 
 program
     .action(async (data) => {
+        const started = Date.now();
         try {
             // --- if no token; then return
             if (!data.token) {
-                console.error(`user github token is required; ${moreInfo}`);
+                console.error(
+                    error,
+                    `user github token is required; ${moreInfo}`
+                );
                 process.exit();
             }
 
             // --- name of the file
+            console.log(success, "processing filename & output path");
             let name = fileName(data.name);
 
+            // --- path
+            let path = data.path + `./${name}`;
+
             // --- get user info
+            console.log(info, "fetching user info from token");
             const user = await Rest.getUserDetails(data.token);
+            console.log(success, "fetched user info from token");
 
             // --- get list of all user repos
+            console.log(info, "fetching list of all user repos");
             const repos = await repolist(data.token);
+            console.log(success, "fetched list of all user repos");
 
             // --- download all repos
-            await download(repos, user, data.token, "./repos");
+            console.log(info, "downloading repos");
+            await download(repos, user, data.token, path);
+            console.log(success, "downloaded all repos");
+
+            // --- compress backup
+            console.log(info, "compressing repos");
+            await compress(path, `${name}.zip`);
+            console.log(success, "compressed all repos");
+
+            console.log(
+                success,
+                `completed successfully in ${Date.now() - started}s`
+            );
         } catch (err) {
             console.error(err?.message);
             process.exit();
